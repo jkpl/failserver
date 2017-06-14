8a84d91a024726ee1958fd08029d2c9f9fccc42d
Jaakko Pallari <jaakkop@cakesolutions.net>
2017-06-14 15:35:54 +0100

    Randomly failing HTTP server

--- failserver.go
+++ failserver.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch n := rand.Intn(100); n {
+	case 4:
+		http.Error(w, "Could not find your lucky number!", http.StatusNotFound)
+	case 5:
+		http.Error(w, "Failed to compute your lucky number!", http.StatusInternalServerError)
+	default:
+		fmt.Fprintf(w, "Your lucky number is %d", n)
+	}
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
